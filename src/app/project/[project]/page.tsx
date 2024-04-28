911096a58863ede281942c655cc3982c3308c3cb
Patric Stout <truebrain@openttd.org>
2024-04-28 16:02:00 +0200

    chore: move ConfigProvider to layout.tsx, as all pages need it

--- src/app/project/[project]/page.tsx
+++ src/app/project/[project]/page.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { getNileConfig } from "@/static/NileConfig";
-import { ConfigProvider } from "@/providers/ConfigProvider";
 import { ProjectProvider } from "@/providers/ProjectProvider";
 import { LayoutCommon } from "@/components/LayoutCommon";
 import { Listing } from "./Listing";
 
 export default async function Home({ params }: { params: { project: string } }) {
   const { project } = params;
-  const nileConfig = await getNileConfig();
 
   return (
     <main>
-      <ConfigProvider config={nileConfig}>
-        <ProjectProvider initialProject={project}>
-          <LayoutCommon>
-            <Listing />
-          </LayoutCommon>
-        </ProjectProvider>
-      </ConfigProvider>
+      <ProjectProvider initialProject={project}>
+        <LayoutCommon>
+          <Listing />
+        </LayoutCommon>
+      </ProjectProvider>
     </main>
   );
 }
